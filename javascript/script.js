ba932f0caf53055d659881d5bab14655b6a6922a
VIVIAN RION <vivianrionmarandi@gmail.com>
2024-03-03 23:15:40 +0600

    discussion section uploaded for mobile responsive view

--- javascript/script.js
+++ javascript/script.js
@@ -32,31 +32,31 @@ const loadPost = (data) => {
             <div id="greenSignal" class="rounded-full w-[15px] h-[15px] absolute -right-1 -top-1 ${element.isActive ? 'bg-green-600' : 'bg-red-600'} ">
             </div>
         </div>
-        <div class="flex-1">
+        <div class="lg:flex-1">
             <div class="border-b-2 border-neutral-400 border-dashed">
-                <div class="font-inter text-sm font-medium text-neutral-500 flex gap-x-5">
+                <div class="font-inter text-sm font-medium text-neutral-500 flex flex-col lg:flex-row gap-y-2 lg:gap-y-0 lg:gap-x-5">
                     <h6>#<span>${element.category}</span></h6>
                     <h6>Author: <span>${element.author.name}</span></h6>
                 </div>
                 <h3 class="text-xl font-bold mt-3 mb-4">${element.title}</h3>
                 <p class="font-inter text-neutral-400 text-base leading-7 mb-5">${element.description}</p>
             </div>
-            <div class="mt-5 font-inter font-normal text-base text-neutral-500 flex justify-between">
+            <div class="mt-5 font-inter font-normal text-base text-neutral-500 flex flex-col gap-y-6 lg:gap-y-0 lg:flex-row lg:justify-between">
                 <div class="flex gap-x-6">
-                    <div class="flex gap-x-3 items-center">
+                    <div class="flex gap-1 lg:gap-x-3 items-center">
                         <img class="w-5 h-5" src="images/messageicon.png" alt="...Loading">
                         <h6>${element.comment_count}</h6>
                     </div>
-                    <div class="flex gap-x-3 items-center">
+                    <div class="flex gap-1 lg:gap-x-3 items-center">
                         <img class="w-5 h-5" src="images/eyeicon.png" alt="...Loading">
                         <h6>${element.view_count}</h6>
                     </div>
-                    <div class="flex gap-x-3 items-center">
+                    <div class="flex gap-1 lg:gap-x-3 items-center">
                         <img class="w-5 h-5" src="images/clockicon.png" alt="...Loading">
                         <h6>${element.posted_time}</h6>
                     </div>
             </div>
-            <div class="flex gap-x-3 items-center cursor-pointer" onclick='markAsRead("${element.title.replace(/'/g, '')}",${element.view_count})'>
+            <div class="translate-x-40 lg:translate-x-0 flex gap-x-3 items-center cursor-pointer" onclick='markAsRead("${element.title.replace(/'/g, '')}",${element.view_count})'>
                 <img class="w-7 h-7" src="images/emailicon.png" alt="...Loading" id="mark-read">
             </div>
         </div>                        
@@ -143,7 +143,7 @@ const postsPrint = (element) => {
         </div>
         <div class="flex gap-x-3 items-center my-6">
             <img class="w-5 h-5" src="images/date.png" alt="...Loading">
-            <h6 class="text-base font-normal text-neutral-400">${card.author?.posted_date??'No publish date'}</h6>
+            <h6 class="text-base font-normal text-neutral-400">${card.author?.posted_date ?? 'No publish date'}</h6>
         </div>
         <h4 class="text-lg font-extrabold w-80 mb-3">${card.title}</h4>
         <p class="text-neutral-500 w-80 text-start leading-6 font-normal mb-4">${card.description}</p>
@@ -153,7 +153,7 @@ const postsPrint = (element) => {
             </div>
             <div>
                 <h4 class="text-base font-bold text-black mb-1">${card.author.name}</h4>
-                <h6 class="text-sm font-normal text-neutral-400">${card.author.designation??"Unknown"}</h6>
+                <h6 class="text-sm font-normal text-neutral-400">${card.author.designation ?? "Unknown"}</h6>
             </div>
         </div>
         `;
